824fc9b00a472075f1b16852c4ee506ced2202d9
william <williambrunosalesdepaulalima@gmail.com>
2025-04-25 11:13:20 -0300

    feat: adding agent code

---
+++ app/services/agent.py
@@ -0,0 +1,16 @@
+from llama_index.core.agent.workflow import AgentWorkflow
+from app.services.llm_wrapper import llm
+from app.tools.check_hub_stats import check_hub_stats_tool
+from app.tools.check_weather import check_weather_tool
+from app.tools.perform_websearch import duckduckgo_search_tool
+from app.tools.perform_retrieval import perform_retrieval_tool
+
+alfred = AgentWorkflow.from_tools_or_functions(
+    [
+        check_hub_stats_tool, 
+        check_weather_tool, 
+        duckduckgo_search_tool, 
+        perform_retrieval_tool
+    ],
+    llm=llm
+)
